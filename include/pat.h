7f27446eacdaa4b4b23b2103b5b7bc5d48e9055f
wsl wy <leftovers06@outlook.com>
2024-08-22 11:30:45 +0800

    -

--- include/pat.h
+++ include/pat.h
@@ -388,3 +388,5 @@ void pat_a_1018(); // *
 void pat_a_1030(); // *
 
 void pat_a_1072(); // **
+
+void pat_a_1087();
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "pat.h"
 
 int main() {
-    pat_a_1072();
+    pat_a_1087();
 
     return 0;
 }
